f2b3a5f5d91e8d2d2c06a741620c194abf2a3149
louis61619 <louis61619@gmail.com>
2021-09-04 16:49:13 +0800

    feat(storybook): add story book

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+import { app } from '@storybook/vue3'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+app.mount('#app')
